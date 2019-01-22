7e176f5279ce1ddcbf4c9e76c164b75d7b7e4123
Gurwinder Singh <gurwinder1317@gmail.com>
2019-01-22 21:43:35 +0530

    work for navbar

--- js/main.js
+++ js/main.js
@@ -2,34 +2,41 @@ window.sr = ScrollReveal();
 
 covers = document.getElementsByClassName("cover-work")
 for (let index = 0; index < covers.length; index++) {
-   covers[index].style.visibility ="hidden";
+    covers[index].style.visibility = "hidden";
 }
- contents = document.getElementsByClassName("content");
+contents = document.getElementsByClassName("content");
 for (let index = 0; index < contents.length; index++) {
-    contents[index].style.visibility ="hidden";
-    
+    contents[index].style.visibility = "hidden";
+
 }
- works = document.getElementsByClassName("show-work");
- for (let index = 0; index < works.length; index++) {
-    works[index].style.visibility ="hidden";
-    
+works = document.getElementsByClassName("show-work");
+for (let index = 0; index < works.length; index++) {
+    works[index].style.visibility = "hidden";
+
 }
 sr.reveal('.cover-work', {
-    duration:2000,
-    origin:'bottom',
-    distance:'50px'
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
 });
 sr.reveal('.content', {
-    duration:2000,
-    origin:'top',
-    distance:'300px'
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
 });
 
- sr.reveal('.show-work', {
-    duration:2000,
-    origin:'left',
-    distance:'300px'
+sr.reveal('.show-work', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
 });
+
+function toggleMenu(x) {
+    x.classList.toggle("change");
+    menu = document.getElementsByClassName("menu");
+    menu[0].classList.toggle("menu_visible");
+    // menu[0].style.display = menu[0].style.display == "block" ? "none":"block";
+}
 
 // sr.reveal('.showcase-btn', {
 //     duration:2000,
