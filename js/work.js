a0b96b46e8c32ab6b9619c55b034ae7447b16199
Gurwinder Singh <gurwinder1317@gmail.com>
2019-01-25 00:36:06 +0530

    work

--- js/work.js
+++ js/work.js
@@ -1,1 +1,11 @@
 // https://jmperezperez.com/medium-image-progressive-loading-placeholder/
+var wrapper =  document.querySelectorAll(".wrapper");
+wrapper.forEach(e => {
+    var attr = e.dataset.imageSrc;
+
+    if (typeof attr !== typeof undefined && attr !== false) {
+
+        e.style.background= "url("+attr+") center / cover no-repeat";
+    }
+  
+  });
