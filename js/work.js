a3d2a8e3dd3cae476970dfa29bdbf85cb93238cb
Gurwinder Singh <gurwinder1317@gmail.com>
2019-01-25 23:02:44 +0530

    dynamic images in work

--- js/work.js
+++ js/work.js
@@ -9,3 +9,50 @@ wrapper.forEach(e => {
     }
   
   });
+
+let lastScroll = 0;
+
+document.addEventListener("scroll", function winScroll(){
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    if(st > lastScroll){
+        //down scroll
+    }else{
+        //top scroll
+    }
+    lastScroll = st <=0 ? 0 :st; //for mobile or negative scroll
+});
+
+let row = document.querySelector(".row");
+function loadDataFromJson(json) {
+    json.work[0].art.forEach(e =>{
+   var data =  `<div class="card" >
+    <div class="wrapper">
+    <img src="${e.img}" > 
+        <div class="header">
+            <div class="date">${e.time}</div>
+        </div>
+   
+        <div class="content">
+            <div class="author">${e.author}</div>
+            <div class="data">${e.data}</div>
+            <div class="link">
+            ${e.link ? `<a href="${e.link}" target="_blank"> ${e.link_text}</a>`:``}
+            </div>
+        </div>
+    </div>
+</div>`;
+   row.innerHTML += data;
+    });
+}
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+     let json = JSON.parse(this.responseText);
+    // document.getElementById("demo").innerHTML = myObj.name;
+    loadDataFromJson(json);
+
+  }
+};
+xmlhttp.open("GET", "../images/data.json", true);
+xmlhttp.send();
